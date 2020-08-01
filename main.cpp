eaa33167123ad9db428a9eaf2d2abf99deb2e6bb
ZzzhaoGZ <1569389459@qq.com>
2020-08-01 18:17:27 +0800

    =

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
+using namespace std;
 #include "1.h"
 #include "Fbi.h"
 #include "jo.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    long long i;
-    while(cin>>i)
-    {
-        O_or_j(i);
-    }
-    return 0;
+    cout<<"赵港是你爹"<<endl;
 }
 

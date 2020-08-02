9ec193afcd64fef392a7252bbe4d108886b872ff
ZzzhaoGZ <1569389459@qq.com>
2020-08-02 23:01:14 +0800

    chuan_index.h

--- chuan_index.h
+++ chuan_index.h
@@ -5,4 +5,25 @@
 #ifndef UNTITLED_CHUAN_INDEX_H
 #define UNTITLED_CHUAN_INDEX_H
 
+
+int Index(String S, String T, int pos)
+{
+    int n,m,i;
+    String sub;
+    if (pos>0)
+    {
+        n = StrLength(S);
+        m = StrLength(T);
+        i = pos;
+        while (i<=n-m+1)
+        {
+            SubString(sub,S,i,m);
+            if (StrCompare(sub,T)!=0)
+                ++i;
+            else
+                return i;
+        }
+    }
+    return 0;
+}
 #endif //UNTITLED_CHUAN_INDEX_H

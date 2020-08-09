4d7a89a879448c93639be4f48b1c9bf864e4ed5d
ZzzhaoGZ <1569389459@qq.com>
2020-08-09 21:17:22 +0800

    2020年08月09日21:04:12 2020年08月09日21:17:19

--- 排序.h
+++ 排序.h
@@ -6,7 +6,7 @@
 #define UNTITLED_排序_H
 
 void insertsort(int arr[],int n)
-{
+{//直接插入排序
     int temp,i,j;
     for(i=1;i<n;++i)
     {
@@ -21,4 +21,19 @@ void insertsort(int arr[],int n)
     }
 }
 
+void selectsort(int arr[],int n)
+{
+    int i,j,k;
+    int temp;
+    for(i=0;i<n;++i)
+    {
+        k=i;
+        for(j=i+1;j<n;++j)
+            if(arr[k]>arr[j])
+                k=j;
+        temp=arr[i];
+        arr[i]=arr[k];
+        arr[k]=temp;
+    }
+}
 #endif //UNTITLED_排序_H

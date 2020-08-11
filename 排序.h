fd6993abd13004e7c886045ce8cc7dbd24effbcf
ZzzhaoGZ <1569389459@qq.com>
2020-08-11 23:22:28 +0800

    2020年08月11日23:22:25

--- 排序.h
+++ 排序.h
@@ -56,6 +56,22 @@ void bublesort(int arr[],int n)
             return;
     }
 }//sgjj
+
+void shellsort(int arr[], int n)
+{
+    int temp;
+    for (int gap = n/2; gap > 0; gap/=2)
+    {
+        for (int i = gap; i < n; ++i)
+        {
+            temp=arr[i];
+            int j;
+            for(j=i;j>=gap&&arr[j-gap]>temp;j-=gap)
+                arr[j]=arr[j-gap];
+            arr[j]=temp;
+        }
+    }
+}
 #endif //UNTITLED_排序_H
 
 

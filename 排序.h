9e55cc51c7e26901e92b2eb1ee91d95fdf63c6b8
ZzzhaoGZ <1569389459@qq.com>
2020-08-20 22:07:15 +0800

    更新归并排序

--- 排序.h
+++ 排序.h
@@ -136,6 +136,44 @@ void heapSort(int arr[],int n)
     }
 }
 
+
+void merge(int arr[],int low,int mid,int high)
+{//归并排序所需函数
+
+    int i,j,k;
+    int n1=mid-low+1;
+    int n2=high-mid;
+    int L[n1],R[n2];
+    for(i=0;i<n1;i++)
+        L[i]=arr[low+i];
+    for(j=0;j<n2;j++)
+        R[j]=arr[mid+1+j];
+    i=0;
+    j=0;
+    k=low;
+    while(i<n1&&j<n2)
+    {
+        if(L[i]<=R[j])
+            arr[k]=L[i++];
+        else
+            arr[k]=R[j++];
+        k++;
+    }
+    while (i<n1)
+        arr[k++]=L[i++];
+    while (j<n2)
+        arr[k++]=R[j++];
+}
+void mergeSort(int arr[],int low,int high)
+{
+    if(low<high)
+    {
+        int mid = (low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+}
 #endif //UNTITLED_排序_H
 
 
